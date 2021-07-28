627253555251a8ac7ed321767ab31f78aa3427cd
吳妍霈 <86469302+WuYenPei@users.noreply.github.com>
2021-07-28 18:53:44 +0800

    Add files via upload

---
+++ 0724.js
@@ -0,0 +1,10 @@
+// 7月15日上課練習
+
+function displayName(firstName, lastName) {
+  
+  
+}
+  displayName("Hello", "Kitty")   // 印出 "Hello Kitty"
+  displayName("Hello", "天線寶寶") // 印出 "Hello 天線寶寶"
+  displayName("Hello")            // 印出 "Hello Kitty"
+  displayName()                   // 印出 請輸入名稱
