2b12bfd1b7026205099c16c13df85037bf21bd84
吳妍霈 <86469302+WuYenPei@users.noreply.github.com>
2021-07-29 20:52:55 +0800

    Add files via upload

---
+++ 0729上課/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#productArea").addEventListener("click", (e) => {
+      e.preventDefault()
+      
+  
+    })
+  })
